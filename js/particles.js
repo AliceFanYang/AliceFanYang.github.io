b7c7e4bf56687b1253516ccb039228872c3d7689
Alice Yang <ayang@smith.edu>
2016-11-03 11:01:14 -0400

    Revert last commit

---
+++ js/particles.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	particleground(document.getElementById('particles'), {
+	  dotColor: '#FFC0CB',
+	  lineColor: '#FFC0CB'
+	});
+}, false);
+
