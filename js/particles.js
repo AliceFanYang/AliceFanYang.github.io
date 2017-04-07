1d313beee82dcc2d0d730292ca20cc712bb5ebd7
Alice Yang <ayang@smith.edu>
2017-04-06 20:27:49 -0400

    updates

--- js/particles.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	particleground(document.getElementById('particles'), {
-	  dotColor: '#FFC0CB',
-	  lineColor: '#FFC0CB'
-	});
-}, false);
-
